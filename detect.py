20a1c54aceefe27e23624b3a59a75daa7ada55f3
Paul Klemstine <paulklemstine@yahoo.com>
2019-10-23 21:54:30 -0500

    only upright

--- detect.py
+++ detect.py
@@ -334,9 +334,8 @@ if __name__ ==  '__main__':
                 #crop_img=img[c1[1],c1[1]+hei,c1[0]-letterbox:c1[0]-letterbox+hei]
                 bbox=c1[0]-letterbox,c1[1],c1[0]-letterbox+hei,c1[1]+hei
                 crop_image=imcrop(img,bbox)
-                
-            newimage = cv2.resize(crop_image,(512,512))
-            cv2.imwrite("{}/sub_{}".format(args.det,str(uuid.uuid1())+".jpg"),newimage)
+                newimage = cv2.resize(crop_image,(512,512))
+                cv2.imwrite("{}/sub_{}".format(args.det,str(uuid.uuid1())+".jpg"),newimage)
             
         #cv2.rectangle(img, c1, c2,color, 1)
         #t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
