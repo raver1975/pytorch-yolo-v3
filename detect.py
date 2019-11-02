b2ca03c043f03a59fb4c84c289158c4cb14c56ad
Paul Klemstine <paulklemstine@yahoo.com>
2019-11-02 12:15:12 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -300,13 +300,11 @@ if __name__ ==  '__main__':
         # resize the input image, adding a single column (width) so we
         # can compute the horizontal gradient
         resized = cv2.resize(image, (hashSize + 1, hashSize))
- 
         # compute the (relative) horizontal gradient between adjacent
         # column pixels
         diff = resized[:, 1:] > resized[:, :-1]
- 
-	    # convert the difference image to a hash
-	    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+        # convert the difference image to a hash
+        return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
     
     def write(x, batches, results):
         c1 = tuple(x[1:3].int())
